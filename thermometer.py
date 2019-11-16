77c4f52bbd2f5c320c47a4ca2d09d6ade80ec632
Vasile Barna <vasi_barna@yahoo.com>
2019-11-16 12:10:55 +0200

    update

--- thermometer.py
+++ thermometer.py
@@ -1,3 +1,25 @@
-class cluj-termometru:
-	def __init__(self, temperature):
-		self.temperaure = temperature
+class Thermometer:
+
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def get_temperature(self, unit = "C"):
+        if unit == "C":
+            return self.temperature
+        elif unit == "F":
+            self.to_fahrenheit(self.temperature)
+        return self.temperature
+
+    def set_temperature(self, temperature):
+        self.temperature = temperature 
+
+    def to_celsius(self, temperature):
+        temperature = (temperature - 32) * 5 / 9
+        return temperature
+
+    def to_fahrenheit(self, temperature):
+        temperature = temperature * 9 / 5 + 32
+        return temperature
+
+
+
